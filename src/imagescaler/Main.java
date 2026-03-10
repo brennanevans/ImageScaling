1ddaeab1a15e679ac421005e82ac635f07dd8d54
Brennan <116033545+brennanevans@users.noreply.github.com>
2026-03-10 21:19:13 +0000

    Documented Main class
    Main class just shows examples of how to call the image scaler. Added a comment to reflect this.

--- src/imagescaler/Main.java
+++ src/imagescaler/Main.java
@@ -1,11 +1,8 @@
 package imagescaler;
 
-// Currently in .gitignore as this is just the method to test the classes 
 public class Main {
+    // An example of how to use the ImageScaler
     public static void main(String[] args) {
         Scaler.scaleImage("src\\testImage.png","output","png", 3);
     }
 }
-
-
-// Make work with png alpha values (colour.getAlpha(), interpolate alpha values? int[][][] but last list is of size 4 now?)

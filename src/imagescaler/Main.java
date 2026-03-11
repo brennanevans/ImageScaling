9a85b4db904821733ee9de4d3d4e55f58d865ec0
Brennan <brennan.peter.evans@gmail.com>
2026-03-11 16:41:29 +0000

    Refactored Scaler class to allow for proper scaling of images containing alpha values.
    Also improved readaility of Scaler class.

--- src/imagescaler/Main.java
+++ src/imagescaler/Main.java
@@ -1,11 +1,8 @@
 package imagescaler;
 
-<<<<<<< HEAD
 import java.io.IOException;
 
 // Currently in .gitignore as this is just the method to test the classes 
-=======
->>>>>>> 387a262ecb9dd6087486ab3bef9441f634295427
 public class Main {
     // An example of how to use the ImageScaler
     public static void main(String[] args) {
@@ -16,8 +13,4 @@ public class Main {
         }
         
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 387a262ecb9dd6087486ab3bef9441f634295427
